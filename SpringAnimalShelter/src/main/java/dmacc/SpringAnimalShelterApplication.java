ba7a0858dd9fb3bed86b329f29f3d9c425962448
Kevin Roney <karoney@dmacc.edu>
2023-04-04 20:13:56 -0500

    Removed H2 dependency, removed spring-boot-starter-test dependency

--- SpringAnimalShelter/src/main/java/dmacc/SpringAnimalShelterApplication.java
+++ SpringAnimalShelter/src/main/java/dmacc/SpringAnimalShelterApplication.java
@@ -30,8 +30,10 @@ public class SpringAnimalShelterApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		ApplicationContext appContext = new	AnnotationConfigApplicationContext(BeanConfiguration.class);
 		
-		Owner o1 = appContext.getBean("owner", Owner.class);
-		Owner o2 = appContext.getBean("owner", Owner.class);
+//		Owner o1 = appContext.getBean("owner", Owner.class);
+//		Owner o2 = appContext.getBean("owner", Owner.class);
+		Owner o1 = new Owner();
+		Owner o2 = new Owner();
 		
 		o1.setOwnerId(1);
 		o1.setOwnerName("Kevin");
@@ -41,32 +43,35 @@ public class SpringAnimalShelterApplication implements CommandLineRunner {
 		o2.setOwnerName("Kati");
 		o2.setPetCount(3);
 
-		Pet pet1 = new Pet("Haven", "Siberian Husky", 5, o1);
-		repo.save(pet1);
+//		Pet pet1 = new Pet("Haven", "Siberian Husky", 5, o1);
+//		repo.save(pet1);
 //		Pet p1 = appContext.getBean("pet", Pet.class);
-//		p1.setName("Haven");
-//		p1.setBreed("Siberian Husky");
-//		p1.setAge(5);
-//		p1.setOwner(o);
-//		repo.save(p1);
-
-		Pet pet2 = new Pet("Havek", "German Shepherd", 3, o1);
-		repo.save(pet2);
+		Pet p1 = new Pet();
+		p1.setName("Haven");
+		p1.setBreed("Siberian Husky");
+		p1.setAge(5);
+		p1.setOwner(o1);
+		repo.save(p1);
+		
+//		Pet pet2 = new Pet("Havek", "German Shepherd", 3, o1);
+//		repo.save(pet2);
 //		Pet p2 = appContext.getBean("pet", Pet.class);
-//		p2.setName("Havek");
-//		p2.setBreed("German Shepherd");
-//		p2.setAge(3);
-//		p2.setOwner(o);
-//		repo.save(p2);
+		Pet p2 = new Pet();
+		p2.setName("Havek");
+		p2.setBreed("German Shepherd");
+		p2.setAge(3);
+		p2.setOwner(o1);
+		repo.save(p2);
 
-		Pet pet3 = new Pet("Zuli", "Siberian Husky", 2, o1);
-		repo.save(pet3);
+//		Pet pet3 = new Pet("Zuli", "Siberian Husky", 2, o1);
+//		repo.save(pet3);
 //		Pet p3 = appContext.getBean("pet", Pet.class);
-//		p3.setName("Zuli");
-//		p3.setBreed("Siberian Husky");
-//		p3.setAge(2);
-//		p3.setOwner(o);
-//		repo.save(p3);
+		Pet p3 = new Pet();
+		p3.setName("Zuli");
+		p3.setBreed("Siberian Husky");
+		p3.setAge(2);
+		p3.setOwner(o1);
+		repo.save(p3);
 
 		Pet pet4 = new Pet("Lexi", "Black Lab", 3, o2);
 		repo.save(pet4);
